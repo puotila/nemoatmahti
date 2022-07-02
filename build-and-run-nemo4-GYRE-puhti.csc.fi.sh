ad79d3dabd40c4010b919f99d4adf73278d76e5e
Petteri Uotila <petteri.uotila@helsinki.fi>
2022-07-02 15:16:07 +0300

    intial comimt

--- build-and-run-nemo4-GYRE-puhti.csc.fi.sh
+++ build-and-run-nemo4-GYRE-puhti.csc.fi.sh
@@ -2,81 +2,81 @@
 
 set -ex
 
-# NEMO build and install instructions for puhti.csc.fi
+# NEMO build and install instructions for mahti.csc.fi
 #
 # 2018-12-11, Juha Lento, CSC
 # 2018-12-14, Petteri Uotila, INAR/UH
+# 2022-06-30, Petteri Uotila, INAR/UH
 
-nemo_version=4.0
+PROJ=project_2000789 
 
-compiler=intel
-compiler_version=19.0.4
-mpi=intel-mpi
-mpi_version=18.0.5
+nemo_version=4.2.0
+
+compiler=gcc
+compiler_version=11.2.0
+mpi=mpich
+mpi_version=4.0.1
 
 module purge
 module load StdEnv ${compiler}/${compiler_version} ${mpi}/${mpi_version}
-module load netcdf/4.7.0 netcdf-fortran/4.4.4 hdf5/1.10.4-mpi
+module load boost/1.77.0-mpi
+module load netcdf-c/4.8.1 netcdf-fortran/4.5.3
 
-cd $PROJAPPL
+cd /scratch/$PROJ/$USER
 # Checkout sources
-svn co https://forge.ipsl.jussieu.fr/nemo/svn/NEMO/releases/r4.0/r4.0.6
-#svn co https://forge.ipsl.jussieu.fr/nemo/svn/NEMO/releases/release-4.0
-#svn co -r10645 http://forge.ipsl.jussieu.fr/nemo/svn/NEMO/trunk nemo4.0
 
-cd r4.0.6
-nemo_revision=$(svn info | sed -n 's/Revision: \([0-9]\+\)/\1/p')
+git clone --branch $nemo_version https://forge.nemo-ocean.eu/nemo/nemo.git nemo_$nemo_version
+cd nemo_$nemo_version
 
-cat > arch/arch-${compiler}-puhti.csc.fi.fcm <<EOF
-%CC                  mpicc
-%CFLAGS              -O0
-%CPP                 cpp
-%FC                  mpif90
-%FCFLAGS             $(case $compiler in (gnu) echo '-fdefault-real-8 -O3 -funroll-all-loops -fcray-pointer -ffree-line-length-none';; (intel) echo '-O3 -i4 -r8 -fp-model precise -fno-alias -fpp';; esac)
+cat > arch/arch-${compiler}-mahti.csc.fi.fcm <<EOF
+%HDF5_HOME           /appl/spack/v017/install-tree/gcc-11.2.0/hdf5-1.10.7-veijpf
+%NCDF_C_HOME         $NETCDF_C_INSTALL_ROOT
+%NCDF_F_HOME         $NETCDF_FORTRAN_INSTALL_ROOT
+%XIOS_HOME           /projappl/project_2000789/puotila/XIOS/trunk
+
+%NCDF_INC            -I%NCDF_F_HOME/include -I%NCDF_C_HOME/include -I%HDF5_HOME/include
+%NCDF_LIB            -L%NCDF_F_HOME/lib -lnetcdff -L%NCDF_C_HOME/lib -lnetcdf
+%XIOS_INC            -I%XIOS_HOME/inc 
+%XIOS_LIB            -L%XIOS_HOME/lib -lxios -lstdc++ 
+
+%CPP	             cpp -Dkey_nosignedzero 
+%FC	             mpif90 -c -cpp
+%FCFLAGS             -fdefault-real-8 -O1 -funroll-all-loops -fcray-pointer -ffree-line-length-none
 %FFLAGS              %FCFLAGS
 %LD                  mpif90
-%LDFLAGS
-%FPPFLAGS            -P -C -traditional-cpp
+%LDFLAGS             -lstdc++
+%FPPFLAGS            -P -C -traditional
 %AR                  ar
 %ARFLAGS             rs
-%MK                  make
-
-%NCDF_HOME           $NETCDF_INSTALL_ROOT
-%NCDFF_HOME          $NETCDF_FORTRAN_INSTALL_ROOT
-%HDF5_HOME           $HDF5_INSTALL_ROOT
-%XIOS_HOME           $PROJAPPL/xios-2.5
-%OASIS_HOME          /not/defined
-%NCDF_INC            -I%NCDF_HOME/include -I%NCDFF_HOME/include
-%NCDF_LIB            -L%NCDF_HOME/lib -L%NCDFF_HOME/lib -lnetcdff -lnetcdf -L%HDF5_HOME/lib -lhdf5_hl -lhdf5 -lhdf5
-%XIOS_INC            -I%XIOS_HOME/inc
-%XIOS_LIB            -L%XIOS_HOME/lib -lxios -lstdc++
-
+%MK                  gmake
 %USER_INC            %XIOS_INC %NCDF_INC
 %USER_LIB            %XIOS_LIB %NCDF_LIB
+
+%CC                  gcc
+%CFLAGS              -O0
 EOF
 
-./makenemo -j 8 -m ${compiler}-puhti.csc.fi -r GYRE_PISCES -n MY_GYRE del_key key_top
+./makenemo -j 8 -m ${compiler}-mahti.csc.fi -r GYRE_PISCES -n MY_GYRE del_key key_top
 
 # run the experiment
 cd cfgs/MY_GYRE/EXP00
 
 sbatch << EOF
-#!/bin/bash -l
+#!/bin/bash
 ###
-### parallel job script example
-###
-## name of your job
-#SBATCH --job-name=gyre
+#SBATCH --job-name=nemo
 #SBATCH --account=project_2000789
-#SBATCH --time=00:15:00
-#SBATCH --mem-per-cpu=2G
+#SBATCH --time=00:30:00
+##SBATCH --mem-per-cpu=2G
+#SBATCH --nodes=1
 #SBATCH --partition=test
+##SBATCH --ntasks-per-node=16
 
 module purge
 module load StdEnv ${compiler}/${compiler_version} ${mpi}/${mpi_version}
-module load netcdf/4.7.0 netcdf-fortran/4.4.4 hdf5/1.10.4-mpi
-
+module load boost/1.77.0-mpi netcdf-c/4.8.1 netcdf-fortran/4.5.3
 ## run my MPI executable
 srun ./nemo
+
 EOF
 
