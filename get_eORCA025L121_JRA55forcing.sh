5e01663f775a8c2f6268cb7febff9c6180a192c0
Petteri Uotila <petteri.uotila@helsinki.fi>
2022-09-06 08:44:24 +0300

    various updates related to eORCA025

--- get_eORCA025L121_JRA55forcing.sh
+++ get_eORCA025L121_JRA55forcing.sh
@@ -3,10 +3,10 @@
 URL='https://ige-meom-opendap.univ-grenoble-alpes.fr/thredds/fileServer/meomopendap/extract/eORCA025.L121/eORCA025.L121-BLD/FORCING'
 WGET='wget -nc'
 
-${WGET} ${URL}/eORCA025_JRA55_do_c3.0_weights_bilin.nc
-${WGET} ${URL}/eORCA025_JRA55_do_c3.0_weights_bilin.nc
+#${WGET} ${URL}/eORCA025_JRA55_do_c3.0_weights_bilin.nc
+#${WGET} ${URL}/eORCA025_JRA55_do_c3.0_weights_bilin.nc
 
-URL='https://ige-meom-opendap.univ-grenoble-alpes.fr/thredds/fileServer/meomopendap/extract/eORCA025.L121/eORCA025.L121-OMP026/forcing'
+URL='https://ige-meom-opendap.univ-grenoble-alpes.fr/thredds/fileServer/meomopendap/extract/eORCA025.L121/eORCA025.L121-OPM026/forcing'
 ${WGET} ${URL}/drowned_uas_JRA55_y1958.nc
 ${WGET} ${URL}/drowned_vas_JRA55_y1958.nc
 ${WGET} ${URL}/drowned_rsds_JRA55_y1958.nc
--- get_eORCA025L121_input.sh
+++ get_eORCA025L121_input.sh
@@ -9,7 +9,7 @@ ${WGET} ${URL}/eORCA025.L121_domain_cfg_b0.2_c3.0_d1.0.nc
 ${WGET} ${URL}/eORCA025_runoff_b0.2_v0.0.nc
 ${WGET} ${URL}/eORCA025.L121_WOA2018_b0.2_c3.0_d1.0_v19812010.5.nc
 ${WGET} ${URL}/eORCA025_seaice_c3.0_v19802004.0.nc
-${WGET} ${URL}/eORCA025_sss_WOA2018_c3.0_d1.0_v19812010.5.nc
+${WGET} ${URL}/eORCA025_sss_WOA2018_c3.0_v19812010.5.nc
 ${WGET} ${URL}/eORCA025_calving_b0.2_v2.3.nc
 ${WGET} ${URL}/ghflux_v2.0.nc
 ${WGET} ${URL}/eORCA025_ghflux_v2.0_c3.0_weights_bilin.nc
