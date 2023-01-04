e9542da1ab1f85c268165616923c48419e1a5338
Petteri Uotila <petteri.uotila@helsinki.fi>
2023-01-04 20:42:06 +0200

    adjusting settings

--- nemorun_eorca025.sh
+++ nemorun_eorca025.sh
@@ -7,8 +7,8 @@
 #SBATCH --account=project_2004927
 #SBATCH --time=01:00:00
 #SBATCH --nodes=2
-#SBATCH --partition=medium
-#SBATCH --ntasks-per-node=128
+#SBATCH --partition=test
+#SBATCH --ntasks-per-node=97
 
 module purge
 module load gcc/11.2.0 openmpi/4.1.2 boost/1.77.0-mpi netcdf-c/4.8.1 netcdf-fortran/4.5.3 hdf5/1.10.7-mpi
