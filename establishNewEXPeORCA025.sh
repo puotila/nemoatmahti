39cf89efd3ae098b818575ada56f1af23c313530
Petteri Uotila <petteri.uotila@helsinki.fi>
2023-01-08 15:20:41 +0200

    making neater and a bug fix

--- establishNewEXPeORCA025.sh
+++ establishNewEXPeORCA025.sh
@@ -17,22 +17,18 @@ SCRATCH=/scratch/${PROJ}
 ln -s ${SCRATCH}/puotila/nemo_4.0.7/cfgs/eORCA025_HPU/BLD/bin/nemo.exe nemo
 cp -p ${PROJAPPL}/puotila/XIOS/trunk/bin/xios_server.exe .
 
-ln -s ../../SHARED/namelist_ref .
-ln -s ../../SHARED/namelist_ice_ref .
-ln -s ../../SHARED/grid_def_nemo.xml .
-ln -s ../../SHARED/field_def_nemo-ice.xml .
-ln -s ../../SHARED/field_def_nemo-oce.xml .
-ln -s ../../SHARED/domain_def_nemo.xml .
-ln -s ../../SHARED/axis_def_nemo.xml .
+for fn in namelist_ref namelist_ice_ref grid_def_nemo.xml field_def_nemo-ice.xml field_def_nemo-oce.xml domain_def_nemo.xml axis_def_nemo.xml
+do
+    ln -s ../../SHARED/${fn} .
+done
 ln -s ${SCRATCH}/nemoinput/eORCA025/eORCA_R025_zps_domcfg_hmin-5.nc .
 
 cp -p ${NEMOATMAHTIREPO}/namelist_ice_cfg.eorca025 namelist_ice_cfg
 cp -p ${NEMOATMAHTIREPO}/namelist_cfg.eorca025 namelist_cfg
-cp -p ${NEMOATMAHTIREPO}/file_def_nemo-oce.xml .
-cp -p ${NEMOATMAHTIREPO}/file_def_nemo-ice.xml .
-cp -p ${NEMOATMAHTIREPO}/context_nemo.xml .
-cp -p ${NEMOATMAHTIREPO}/iodef.xml .
+
 echo "                               0  0.0000000000000000E+00  0.0000000000000000E+00" > EMPave_old.dat
 
-cp -p ${NEMOATMAHTIREPO}/puotila/nemoatmahti/nemorun_eorca025.sh .
-cp -p ${NEMOATMAHTIREPO}/puotila/nemoatmahti/master_eorca025.cfg .
+for fn in file_def_nemo-oce.xml file_def_nemo-ice.xml context_nemo.xml iodef.xml nemorun_eorca025.sh master_eorca025.cfg
+    cp -p ${NEMOATMAHTIREPO}/${fn} .
+do
+
